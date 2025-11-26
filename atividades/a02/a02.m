41d9276b02a8608047d9c61c23fd9aec2a24c9ba
mercedes <mercy.diniz82@gmail.com>
2025-11-26 20:54:19 -0300

    feat: Implementação da A02-parte1

--- atividades/a02/a02.m
+++ atividades/a02/a02.m
@@ -1,4 +1,4 @@
-%% Atividade 02 - CDS
+%% Atividade 02 (Parte 1) - CDS
 % Resolvar o problema de alocação e teste sobre a plata G(s), mostrando que uma escolha de tau_mf de projeto (diferente de 10)
 % funciona com seu controle RST sintetizado;
 clear all; close all; clc;
@@ -6,64 +6,68 @@ clear all; close all; clc;
 tau = 10;
 Gs = tf(1, [ tau 1]) 
 
-Ts = 1; % s
+% Discretizando a planta
+Ts = 1; % periodo de amostragem (s)
 Gz = c2d(Gs, Ts) % G(z) = ( B(z)z{-1} )/A(z) = b0/( 1+a1*z{-1} )
-    b0 = 1-exp(-Ts/tau)
-    a1 = exp(-Ts/tau)
+    Az = Gz.den{1}; a1 = Az(2);     % a1 = exp(-Ts/tau)
+    Bz = Gz.num{1}; b0 = Bz(2);     % b0 = 1-exp(-Ts/tau)
     
 
-%% Comportamento Desejado
-% G_MF_desejado(z) = ( B(z)*T(z)*z^(-d) ) / ( A(z)*R(z)+B(z)*S(z)*z^(-d) )
-% G_MF_desejado(z) = N(z) / Hc(z)
-tau_mf = 15;
-d = 1;
-
-Gs_MF_desejado = tf(1, [ tau_mf 1])
-Gz_MF_desejado = c2d(Gs_MF_desejado, Ts)
-    % b0 = 1-exp(-Ts/tau_mf)
-    % a1 = exp(-Ts/tau_mf)
-
-% H(z) = Hc(z)*Ho(z)
-    % Polinomio desejado em M.F: Ho(z) := A(z) -> quando os polos são estaveis
-    % Polinomio de "observador": Hc(z)
-
-% T(z) = t_off*Ho(z)
-    % t_off = Ho(z->1)/B(z->1) = A(1)/B(1)
-    % t_off = (1 + a1) / b0;
-
-% Eq. Diofantina
-    % A(z)*R(z) + B(z)+S(z)*z^{-d} = 1 + h1*z{-1} + h2*z{-1}
-   
-
-%% RST
+%% Método de controle RST baseado em Astrom e Wittenmark (2011)
 % R(z)*u(k) = T(z)*ref(k) - S(z)*y(k)
 
-tfinal = 20;                % tempo total da simulação (s)
-N = round( tfinal/Ts );     % numero total de amostras
+% Polinômio de atribuição de polos: H(z) = Hc(z)*Ho(z)
 
-% Sinal de referencia
-ref(1:10) = 0; ref(round(N/2):N) = 1; 
+    % Polinomio de "observador": Ho(z) := A(z) -> quando os polos são estaveis
+    Ho = Az;
 
-% for k = 2:N
-% 
-%     % Modelo linear
-%     y(k) = 
-% 
-%     % Controle
-%     e(k) = ref(k) - y(k);
-% 
-%     % u(k) =
-% end
+    % Polinomio desejado em M.F: Hc(z)
+    tau_mf = 15; % constante de tempo de malha fechada
+    Gdes = tf(1,[tau_mf  1]);
+    Gdesz = c2d(Gdes,Ts);
+    Hc = Gdesz.den{1};
 
+    H = conv(Hc,Ho);
+        h1 = H(2); h2 = H(3);
 
+% Polinômio R(z)
+    r0 = 1;
+    r1 = -1;
 
+% Polinômio S(z)
+    s0 = (h1 -a1+1)/b0;
+    s1 = (h2 +a1)/b0;
 
+% Polinômio T(z)
+    % T(z) = t_off*Ho(z)
+    % t_off = Hc(z->1)/B(z->1) = A(1)/B(1)
+    t_off = sum(Hc)/sum(Bz); % pré-compensador DC
+    Tz = t_off*Ho; 
+        t0 = Tz(1); t1 = Tz(2);
 
+%% Simulação no domínio do tempo discreto
+    tfinal = 100;            % tempo total da simulação (s)
+    N = round(tfinal/Ts);   % numero de amostras
+    t = 0:Ts:N*Ts-Ts;       % vetor de tempo discreto
+    
+    % Sinal de referencia
+    ref(1:N) = 0; ref(3:N) = 1; 
+    
+    % Condições iniciais
+    y(1) = 0; u(1) = 0;
 
+for k = 2:N
+    y(k) = -a1*y(k-1) +b0*u(k-1);
 
+    % Controlador RST
+    u(k) = u(k-1) +t0*ref(k) +t1*ref(k-1) -s0*y(k) -s1*y(k-1);
+end
 
-
-
-
-
-
+% Plot
+subplot(211)
+    plot(t,ref,':k',t,y,'b');
+    ylabel('Sinal de saída');
+    legend('Ref.','y');
+subplot(212)
+    plot(t,u,'b');
+    ylabel('Sinal de controle');
