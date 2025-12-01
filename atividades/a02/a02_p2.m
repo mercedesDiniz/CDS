ee40d422c376e22c07d027c78f34c9512a4efa96
mercedes <mercy.diniz82@gmail.com>
2025-12-01 19:51:18 -0300

    test: R(z)=1

--- atividades/a02/a02_p2.m
+++ atividades/a02/a02_p2.m
@@ -18,7 +18,7 @@ tau = 10;
     % y(k) = ( B(z) / ΔA(k) ) * Δu(k)
     % ΔA(k) = ( 1 - z^{-1} ) * ( 1 - a1*z^{-1} )
     delta = [1 -1]; % Δ = 1 - z^{-1} 
-    delta_Az = conv(Az, delta)
+    delta_Az = conv(Az, delta);
         a1_aum = delta_Az(2); a2_aum = delta_Az(3);     
           
 %% Projeto do controlador RST incremental
@@ -32,7 +32,7 @@ tau = 10;
     % Polinomio desejado em M.F: Hc(z)
         % Gs_des = ( g*ωn^{2} ) / ( s^{2}+2ζωns+ ωn^{2} )
         g = 1;          % ganho do sistema
-        wn = 15;        % frequencia natural
+        wn = 1;        % frequencia natural
         zeta = 0.5;     % fator de amortecimento (subamortecido: 0<ζ<1)
 
     Gs_des = tf(g*wn^2, [1  2*zeta*wn   wn^2])
@@ -45,12 +45,12 @@ tau = 10;
 
     % Polinômio R(z)
         r0 = 1;
-        r1 = -1;
+        r1 = 0;
     
     % Polinômio S(z)
-        s0 = (h1 - a1_aum+1)/b0;
-        s1 = (h2 - a2_aum + a1_aum)/b0;
-        s2 = (h3 + a2_aum)/b0;
+        s0 = (h1 - a1_aum)/b0;
+        s1 = (h2 - a2_aum)/b0;
+        s2 = (h3)/b0;
     
     % Polinômio T(z)
     % T(z) = t_off*Ho(z)
@@ -75,7 +75,8 @@ for k = 3:N
     y(k) = b0*u(k)-b0*u(k-1)-a1_aum*y(k-1)-a2_aum*y(k-2);
 
     % Controlador RST
-    u(k) = 2*u(k-1)-u(k-2)+t0*ref(k)+t1*ref(k-1)-s0*y(k)-s1*y(k-1)-s2*y(k-2);
+    % u(k) = 2*u(k-1)-u(k-2)+t0*ref(k)+t1*ref(k-1)-s0*y(k)-s1*y(k-1)-s2*y(k-2);
+    u(k) = u(k-1)+t0*ref(k)+t1*ref(k-1)-s0*y(k)-s1*y(k-1)-s2*y(k-2);
 end
 
 % Plot
