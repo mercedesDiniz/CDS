1fc5b5c6cb81c6cf4757c32d713d19f531ce126a
mercedes <mercy.diniz82@gmail.com>
2026-01-31 11:08:01 -0300

    fix(a05): Filtro de Kalman e LQR finalizados. TODO: n convergiu

--- atividades/a05/a05_plantaA_pmvc.m
+++ atividades/a05/a05_plantaA_pmvc.m
@@ -7,6 +7,7 @@ td = 0;                  % atraso continuo (s)
 d = round((td/Ts) + 1)   % atraso discreto (em amostras)
 
 Nx = d+1;                % horizonte de previsão de estado (atraso virtual)
+LAM = diag([1e6]);       % fator de ponderação de controle
 
 %% Planta a) Dinâmica do ângulo de roll do VLS-1 em Max Q
 
@@ -27,12 +28,12 @@ Nx = d+1;                % horizonte de previsão de estado (atraso virtual)
     [Ad,Bd,Cd,Dd] = ssdata(sysd);
 
 % Modelo aumentado em espaço de estados
-    Aa = A; %[ 1          Cd*Ad;
-         % zeros(2,1)  Ad  ];
+    Aa = [ 1          Cd*Ad;
+         zeros(2,1)  Ad  ];
 
-    Ba = B; %[Cd*Bd  ;  Bd];
+    Ba = [Cd*Bd  ;  Bd];
 
-    Ca = C; %[Cd 0];
+    Ca = [Cd 0];
 
     Da = 0;
 
@@ -61,30 +62,21 @@ disp('Verificação da Observabilidade:');
 
 %% PMVC com realimentação total de estados
 
-%% Estimador de estados baseado no filtro de Kalman
-    Qkf = Ca'*Ca;  % covariancia do ruido do processo
-    Rkf = 1;             % covariancia do ruido de medição
+% Estimador de estados baseado no filtro de Kalman
+    Qkf = diag([1  1 0]);   % covariancia do ruido do processo
+    Rkf = 1e10;             % covariancia do ruido de medição
     
-    % disp('Verificação da Detectabilidade (p/ o Filtro de Kalman):');
-    % Ob_dec_kf = obsv(Aa', sqrt(Qkf)');
-    % disp(['> rank(obsv(Aa^(T), sqrt(Qkf)^(T))) = ' num2str(rank(Ob_dec_kf))]);
-    % 
-    % if rank(Ob_dec_kf) == n
-    %     disp(['== ' num2str(n) ' - O sistema é detectável' ]);
-    % else
-    %     disp(['!= ' num2str(n) ' - O sistema não é detectável' ]);
-    % end
+    disp('Verificação da Detectabilidade (p/ o Filtro de Kalman):');
+    Ob_dec_kf = obsv(Aa', sqrt(Qkf)');
+    disp(['> rank(obsv(Aa^(T), sqrt(Qkf)^(T))) = ' num2str(rank(Ob_dec_kf))]);
 
-    % Calculo da matriz P pela Equação a Diferenças de Riccati
-    % Po = 1e6*eye(n, n); p_samples = 1000; 
-    % for i = 1:p_samples
-    %     Po = Aa*Po*Aa' -Aa*Po*Ca'*inv(Ca*Po*Ca'+Rkf)*Ca*Po*Aa'+Qkf;
-    %     tracePo(i)=trace(Po); 
-    % end
-    % figure; plot(tracePo); ylabel('Traço de P - Filtros de Kalman'); xlabel('Amostras');
+    if rank(Ob_dec_kf) == n
+        disp(['== ' num2str(n) ' - O sistema é detectável' ]);
+    else
+        disp(['!= ' num2str(n) ' - O sistema não é detectável' ]);
+    end
 
     % Calculo do Ganho
-    % L = ( Aa*Po*Ca'*inv(Ca*Po*Ca'+Rkf) );
     L = [ dlqr(Aa',Ca',Qkf,Rkf) ]'
 
     % Verificando os autovalores
@@ -95,30 +87,21 @@ disp('Verificação da Observabilidade:');
     syskf = ss(Aa-L*Ca, L, Ca, Da, Ts);
     figure; step(syskf); title('Kalman-based estimator convergence analysis');
 
-%% LQR
-    Qlqr = Ca'*Ca;  % penalização dos estados
-    Rlqr = 1;                % penalização do esforço de controle
+% LQR
+    Qlqr = diag([1  1 0]);  % penalização dos estados
+    Rlqr = 1e5;             % penalização do esforço de controle
 
-    % disp('Verificação da Detectabilidade (p/ o LQR):');
-    % Ob_dec_lqr = obsv(Aa', sqrt(Qlqr)');
-    % disp(['> rank(obsv(Aa^(T), sqrt(Qlqr)^(T))) = ' num2str(rank(Ob_dec_lqr))]);
-    % 
-    % if rank(Ob_dec_lqr) == n
-    %     disp(['== ' num2str(n) ' - O sistema é detectável' ]);
-    % else
-    %     disp(['!= ' num2str(n) ' - O sistema não é detectável' ]);
-    % end
-    
-    % Calculo da matriz P pela Equação a Diferenças de Riccati
-    % P = 1e6*eye(n, n); p_samples = 1000; 
-    % for i = 1:p_samples
-    %     P = Aa'*P*Aa -Aa'*P*Ba*inv(Ba'*P*Ba+Rlqr)*Ba'*P*Aa+Qlqr;
-    %     traceP(i)=trace(P); 
-    % end
-    %figure; plot(traceP); ylabel('Traço de P - LQR'); xlabel('Amostras');
+    disp('Verificação da Detectabilidade (p/ o LQR):');
+    Ob_dec_lqr = obsv(Aa', sqrt(Qlqr)');
+    disp(['> rank(obsv(Aa^(T), sqrt(Qlqr)^(T))) = ' num2str(rank(Ob_dec_lqr))]);
+
+    if rank(Ob_dec_lqr) == n
+        disp(['== ' num2str(n) ' - O sistema é detectável' ]);
+    else
+        disp(['!= ' num2str(n) ' - O sistema não é detectável' ]);
+    end
 
     % Calculo do Ganho
-    % K = ( Aa'*P*Ba*inv(Ba'*P*Ba+Rlqr) )';
     K =  dlqr(Aa,Ba,Qlqr,Rlqr)
 
     % Verificando os autovalores
@@ -130,9 +113,6 @@ disp('Verificação da Observabilidade:');
     figure; step(syslq); title('LQR servocontrol analysis');
 
     % Variaveis auxiliares
-        % Fator de ponderação de controle
-        LAM = diag([1]); 
-    
         % Matrizes principais da leis de controle
         Mx = K*( Aa^Nx -(Aa^(Nx-1))*L*Ca );
         
@@ -144,58 +124,58 @@ disp('Verificação da Observabilidade:');
         
         My = K*Aa^(Nx-1)*L;
 
-% %% Simulação
-% tfinal = 300;           % tempo de simulação (em segundos)
-% N = round( tfinal/Ts );
-% t = 0:Ts:N*Ts-Ts;      
-% 
-%     % Sinal de referencia
-%     r1(1:Nx+1) = 0; r1(Nx+2:N) = 1; 
-% 
-%     % Condições iniciais
-%     for k = 1:Nx 
-%         x(:,:,k)=[0;0];
-%         u(k) = 0;
-%         y(k) = Cd*x(:,:,k);
-% 
-%         xa(:,:,k) = zeros(3,1);
-%         du(k) = 0;
-%     end
-% 
-% for k = Nx+1:N
-%     % Planta
-%     x(:,:,k) = Ad*x(:,:,k-1) +Bd*u(k-d);
-%     y(k) = Cd*x(:,:,k);
-% 
-%     % Controlador
-%     xa(:,:,k) = (Aa-L*Ca)*xa(:,:,k-1) +Ba*du(k-Nx) +L*y(k-1);
-% 
-%         DU = [];
-%         for i = 1:Nx-1
-%             DU = [DU; du(k-i) ];
-%         end
-% 
-%     du(k) = inv(K*Ba+LAM)*( K*[r1(k);0;0] -Mx*xa(:,:,k) ...
-%         -Mu*DU -My*y(k) );
-% 
-%     u(k) = u(k-1) +du(k);
-% 
-%     % Saturação
-%     if u(k) >= 0.06981
-%       u(k) = 0.06981;
-%     elseif u(k) <= 0
-%       u(k) = 0;
-%     end
-% 
-% end
-% 
-% %% Plots
-% t = 0:Ts:N*Ts-Ts;
-% figure;
-%     subplot(211)
-%     plot(t,r1,':k'); hold;
-%     plot(t,y,'b');
-%     ylabel('y(t)'); xlabel('Time (s)');
-%     subplot(212)
-%     plot(t,du,'b');
-%     ylabel('du(t)'); xlabel('Time (s)');
+%% Simulação
+tfinal = 500;           % tempo de simulação (em segundos)
+N = round( tfinal/Ts );
+t = 0:Ts:N*Ts-Ts;      
+
+    % Sinal de referencia
+    r1(1:Nx+1) = 0; r1(Nx+2:N) = 1*(pi/180); 
+
+    % Condições iniciais
+    for k = 1:Nx 
+        x(:,k)=[0;0];
+        u(k) = 0;
+        y(k) = Cd*x(:,k);
+
+        xa(:,k) = zeros(3,1);
+        du(k) = 0;
+    end
+
+for k = Nx+1:N
+    % Planta
+    x(:,k) = Ad*x(:,k-1) +Bd*u(k-d);
+    y(k) = Cd*x(:,k);
+
+    % Controlador
+    xa(:,k) = (Aa-L*Ca)*xa(:,k-1) +Ba*du(k-Nx) +L*y(k-1);
+
+        DU = [];
+        for i = 1:Nx-1
+            DU = [DU; du(k-i) ];
+        end
+
+    du(k) = inv(K*Ba+LAM)*( K*[r1(k);0;0] -Mx*xa(:,k) ...
+        -Mu*DU -My*y(k) );
+
+    u(k) = u(k-1) +du(k);
+
+    % Saturação
+    if u(k) >= 0.06981
+      u(k) = 0.06981;
+    elseif u(k) <= 0
+      u(k) = 0;
+    end
+
+end
+
+%% Plots
+t = 0:Ts:N*Ts-Ts;
+figure;
+    subplot(211)
+    plot(t,r1,':k'); hold;
+    plot(t,y,'b');
+    ylabel('y(t)'); xlabel('Time (s)');
+    subplot(212)
+    plot(t,du,'b');
+    ylabel('du(t)'); xlabel('Time (s)');
